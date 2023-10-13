d8d8392467e24b94390e3a92890427ef85bb31e5
ssss22s <147750065+ssss22s@users.noreply.github.com>
2023-10-13 22:19:55 +0300

    Update logout.php

--- HTML/logout.php
+++ HTML/logout.php
@@ -7,3 +7,24 @@ header('Location: ../HTML/Login.html');
 exit;
 }
 ?>
+  
+<?php 
+// After User Click On Logout page.
+ require 'database_conn.php'; // Connection
+ session_start();    // Session start
+?>
+
+
+ <?php
+    if(isset($_POST['logout'])) {  
+
+    if(isset($_SESSION['useremail'])){
+
+     unset($_SESSION["useremail"]);
+     session_destroy();
+     session_unset();    
+     header('Location: ../HTML/Login.html');
+
+      }
+    }
+ ?>
