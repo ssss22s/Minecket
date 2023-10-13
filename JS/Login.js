9c0d56086396ddc4e7b1ae8f3d1c1bca74489785
ssss22s <147750065+ssss22s@users.noreply.github.com>
2023-10-13 22:22:49 +0300

    Update Login.js

--- JS/Login.js
+++ JS/Login.js
@@ -1,6 +1,15 @@
 // ===================
 //    LOGIN SYSTEM
 // ===================
+<?php
+if(isset($_GET['logout'])) { 
+
+session_start();
+session_destroy();
+header('Location: ../HTML/Login.html');
+exit;
+}
+?>
 
 function isUserLogged() {
 	const cookie = document.cookie;
