e56ef3095e487f61f7827309588a93c3cf24adcf
ssss22s <147750065+ssss22s@users.noreply.github.com>
2023-10-22 03:10:39 +0300

    Update dd.js

--- JS/dd.js
+++ JS/dd.js
@@ -2,3 +2,16 @@ var loadFile = function (event) {
   var image = document.getElementById("output");
   image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+$('document').ready(function(){
+ setTimeout(function(){
+    $('.notify-container').fadeOut(function(){
+    });
+  }, 10000);
+  $('.show-notify').on( 'click', function(){
+    $('.notify-container').fadeIn();
+  });
+  $('.close-it').on( 'click', function(){
+    $('.notify-container').fadeOut('slow');
+  });
+});
