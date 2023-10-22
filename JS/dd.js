187d097d6a114f26ed974490bd8ac254936bfaa4
ssss22s <147750065+ssss22s@users.noreply.github.com>
2023-10-22 03:18:58 +0300

    Update dd.js

--- JS/dd.js
+++ JS/dd.js
@@ -1,16 +1,9 @@
-window.addEventListener('load', function(){
-  $('.card').each(function() {
-    
-    var imgURL = $(this)
-      .find('.-img')
-      .attr('src');
-    
+window.addEventListener("load", function () {
+  $(".card").each(function () {
+    var imgURL = $(this).find(".-img").attr("src");
+
     $(this)
-      .find('.-background-img')
-      .css('background-image',
-           'url(' + imgURL + ')');
-    
+      .find(".-background-img")
+      .css("background-image", "url(" + imgURL + ")");
   });
 });
-
-  
