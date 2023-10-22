af59928f4491408534b3cc13624241384ff418f0
ssss22s <147750065+ssss22s@users.noreply.github.com>
2023-10-22 04:40:38 +0300

    Update dd.js

--- HTML/dd.js
+++ HTML/dd.js
@@ -1,53 +1,9 @@
-window.URL = window.URL || window.webkitURL;
-blob = true && window.URL;
-function readImg(file, previewImg, width, height, border, hid)
-{
-  var hid1 = document.getElementById(hid);
-  var reader = new FileReader();
-  reader.addEventListener("load", function(){
-    var image = new Image();
-    image.addEventListener("load", function(){
-      Math.round(file.size / 1024) + "KB";
-      previewImg.innerHTML="";
-      previewImg.appendChild(this);
-      if(blob)
-      {
-     window.URL.revokeObjectURL(image.src); 
-      }  
-    });
-    hid1.value = blob ? window.URL.createObjectURL(file) : reader.result;
-      image.src = blob ? window.URL.createObjectURL(file) : reader.result;
-      image.style.width = width + "px";
-      image.style.height = height + "px";
-      image.style.borderRadius = border + "%";
-  });
-  reader.readAsDataURL(file);
-}
 
-function fillImg(fileupload, viewImage, hidImgVal)
-{
-  var fileUploadId = document.getElementById(fileupload);
-  var viewImageId = document.getElementById(viewImage);
-  var files = fileUploadId.files;
-  var error = "";
-  if(!files)
-  {
-    error += "File Upload Not Supported By  Your Browser";
-  }
-  
-  if(files && files[0])
-  {
-      for(var i=0; i<files.length; i++)
-      {
-        var file = files[i];    
-        if ((/\.(png|jpeg|jpg|gif)$/i).test(file.name)) {
-                readImg(file, viewImageId, 50, 50, 50, hidImgVal);
-            } else {
-                errors += file.name + " Unsupported Image extension\n";
-            }
-      }
-  }
-  if (errors) {
-    alert(errors);
-   }
-}
+<div class="profile-pic">
+  <label class="-label" for="file">
+    <span class="glyphicon glyphicon-camera"></span>
+    <span>Change Image</span>
+  </label>
+  <input id="file" type="file" onchange="loadFile(event)"/>
+  <img src="https://cdn.pixabay.com/photo/2017/08/06/21/01/louvre-2596278_960_720.jpg" id="output" width="200" />
+</div>
