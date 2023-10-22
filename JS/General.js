c7d49366b33028895007132dbf6c5aa13675ce03
ssss22s <147750065+ssss22s@users.noreply.github.com>
2023-10-22 03:34:24 +0300

    Update General.js

--- JS/General.js
+++ JS/General.js
@@ -1,6 +1,10 @@
 // ==================
 //   NAVBAR MOBILE
 // ==================
+var loadFile = function (event) {
+  var image = document.getElementById("output");
+  image.src = URL.createObjectURL(event.target.files[0]);
+};
 
 const navbars = document.querySelector('.navbars-bars');
 const navMobileMenu = document.querySelector('.nav-mobile-menu');
