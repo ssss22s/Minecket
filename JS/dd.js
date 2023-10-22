d39b1ff4ca0019ce8955259c55a6b8247de80994
ssss22s <147750065+ssss22s@users.noreply.github.com>
2023-10-22 04:21:30 +0300

    Update dd.js

--- JS/dd.js
+++ JS/dd.js
@@ -1,16 +1,53 @@
- function readURL(input) {
-
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $('#blah').attr('src', e.target.result);
-        }
-
-        reader.readAsDataURL(input.files[0]);
-    }
+window.URL = window.URL || window.webkitURL;
+blob = true && window.URL;
+function readImg(file, previewImg, width, height, border, hid)
+{
+  var hid1 = document.getElementById(hid);
+  var reader = new FileReader();
+  reader.addEventListener("load", function(){
+    var image = new Image();
+    image.addEventListener("load", function(){
+      Math.round(file.size / 1024) + "KB";
+      previewImg.innerHTML="";
+      previewImg.appendChild(this);
+      if(blob)
+      {
+     window.URL.revokeObjectURL(image.src); 
+      }  
+    });
+    hid1.value = blob ? window.URL.createObjectURL(file) : reader.result;
+      image.src = blob ? window.URL.createObjectURL(file) : reader.result;
+      image.style.width = width + "px";
+      image.style.height = height + "px";
+      image.style.borderRadius = border + "%";
+  });
+  reader.readAsDataURL(file);
 }
 
-$("#imgInp").change(function(){
-    readURL(this);
-}) 
+function fillImg(fileupload, viewImage, hidImgVal)
+{
+  var fileUploadId = document.getElementById(fileupload);
+  var viewImageId = document.getElementById(viewImage);
+  var files = fileUploadId.files;
+  var error = "";
+  if(!files)
+  {
+    error += "File Upload Not Supported By  Your Browser";
+  }
+  
+  if(files && files[0])
+  {
+      for(var i=0; i<files.length; i++)
+      {
+        var file = files[i];    
+        if ((/\.(png|jpeg|jpg|gif)$/i).test(file.name)) {
+                readImg(file, viewImageId, 50, 50, 50, hidImgVal);
+            } else {
+                errors += file.name + " Unsupported Image extension\n";
+            }
+      }
+  }
+  if (errors) {
+    alert(errors);
+   }
+}
