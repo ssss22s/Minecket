0880a5f7485e782b5af1e62b08f2ba323cdddeff
ssss22s <147750065+ssss22s@users.noreply.github.com>
2023-11-07 00:05:22 +0300

    Update dd.js

--- JS/dd.js
+++ JS/dd.js
@@ -1,26 +1,10 @@
 <script>
-    const form = document.querySelector("#login-form");
-    const message = document.querySelector("#login-message");
+    function isUserLogged() {
+        const cookie = document.cookie;
+        return cookie.includes("logged=true");
+    }
 
-    form.addEventListener("submit", function (event) {
-        event.preventDefault();
-        const usernameInput = document.querySelector("#username").value.trim();
-        const passwordInput = document.querySelector("#password").value.trim();
-
-        // يمكنك هنا إجراء الاتصال بقاعدة البيانات والتحقق من وجود الحساب
-
-        // إذا كان لديه حساب، يمكن توجيهه إلى صفحة أخرى بعد تسجيل الدخول
-
-        // إذا لم يكن لديه حساب، عرض الرسالة
-        if (!hasAccount(usernameInput, passwordInput)) {
-            message.style.display = "block";
-        }
-    });
-
-    // يمكنك تحديد الدالة التي تتحقق من وجود الحساب هنا
-    function hasAccount(username, password) {
-        // يجب أن تتحقق من وجود الحساب بناءً على قاعدة البيانات أو طريقة أخرى
-        // تعويضيًا، سنقوم بمحاكاة الامر في هذا المثال
-        return username === "user" && password === "password";
+    if (isUserLogged()) {
+        window.location.href = "Cart.html"; // توجيه المستخدم إلى صفحة دخوله
     }
 </script>
