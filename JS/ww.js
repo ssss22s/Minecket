cbf5e21b1daddd0dbfee64504b31aa1108ef8cbb
ssss22s <147750065+ssss22s@users.noreply.github.com>
2023-11-07 00:34:13 +0300

    Update ww.js

--- JS/ww.js
+++ JS/ww.js
@@ -1,14 +1,36 @@
-function isUserLogged() {
-    const cookie = document.cookie;
-    return cookie.includes("logged=true");
+const fs = require('fs'); // يتيح لنا وحدة النظام الأساسي Node.js الوصول إلى نظام الملفات
+
+// اقرأ البيانات من ملف JSON
+function readUserData() {
+    try {
+        const data = fs.readFileSync('data.json', 'utf-8');
+        return JSON.parse(data);
+    } catch (error) {
+        // إذا لم يتم العثور على الملف أو حدث خطأ أثناء القراءة، سنعيد مصفوفة فارغة
+        return [];
+    }
 }
 
-if (isUserLogged()) {
-    window.location.href = "home.html"; // توجيه المستخدم إلى الصفحة الرئيسية بمجرد تسجيل الدخول
+// احفظ البيانات إلى ملف JSON
+function saveUserData(users) {
+    const data = JSON.stringify(users, null, 2); // null و 2 تجعل البيانات مقروءة بشكل أفضل
+    fs.writeFileSync('data.json', data, 'utf-8');
 }
-const date = new Date();
-date.setTime(date.getTime() + 12 * 60 * 60 * 1000);
-const expires = ";expires=" + date.toUTCString() + ";path=/";
 
-// بعد تسجيل الدخول بنجاح
-document.cookie = "logged=true" + expires;
+// احصل على بيانات المستخدمين وقم بتحديثها
+let userDatabase = readUserData();
+
+// اضف البيانات الجديدة إلى قاعدة البيانات
+signupForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const newUsername = document.getElementById('new-username').value;
+    const newPassword = document.getElementById('new-password').value;
+
+    // قم بإضافة المستخدم الجديد إلى قاعدة البيانات
+    userDatabase.push({ username: newUsername, password: newPassword });
+
+    // احفظ البيانات بعد إضافة المستخدم الجديد
+    saveUserData(userDatabase);
+
+    alert('تم إنشاء حساب جديد ويمكنك الآن تسجيل الدخول.');
+});
