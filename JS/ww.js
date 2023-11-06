3967b1a973ee4ca0f0f31f866102b4529a9430d5
ssss22s <147750065+ssss22s@users.noreply.github.com>
2023-11-07 00:02:38 +0300

    Update ww.js

--- JS/ww.js
+++ JS/ww.js
@@ -1,6 +1,14 @@
-if (!isUserLogged()) {
-    // إعطاء رسالة للمستخدم بأنه يجب عليه إنشاء حساب أولاً
-    alert("يجب عليك إنشاء حساب أولاً");
-    // قم بتوجيه المستخدم إلى صفحة إنشاء الحساب
-    window.location.href = "signup.html";
+function isUserLogged() {
+    const cookie = document.cookie;
+    return cookie.includes("logged=true");
 }
+
+if (isUserLogged()) {
+    window.location.href = "home.html"; // توجيه المستخدم إلى الصفحة الرئيسية بمجرد تسجيل الدخول
+}
+const date = new Date();
+date.setTime(date.getTime() + 12 * 60 * 60 * 1000);
+const expires = ";expires=" + date.toUTCString() + ";path=/";
+
+// بعد تسجيل الدخول بنجاح
+document.cookie = "logged=true" + expires;
